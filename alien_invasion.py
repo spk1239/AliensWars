280efd9bb1c1203ff10074b6ad035237339334e3
Birukov Nikita <birukov.na@yandex.ru>
2025-10-06 17:08:33 +0300

    Добавлена кнопка "Play"

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from star import Star
+from button import Button
 import pygame
 from random import randint
 
@@ -33,6 +34,8 @@ class AlienInvasion:
 
         self.bg_color = (230, 230, 230)
 
+        self.play_button = Button(self, "Play")
+
     
     def run_game(self):
 
@@ -57,6 +60,24 @@ class AlienInvasion:
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
 
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+            if button_clicked and not self.stats.game_active:
+                self.stats.reset_stats()
+                self.stats.game_active = True
+
+                self.aliens.empty()
+                self.bullets.empty()
+
+                self._create_fleet()
+                self.ship.center_ship()
+                
+                pygame.mouse.set_visible(False)
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
@@ -117,6 +138,7 @@ class AlienInvasion:
         
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
 
@@ -185,6 +207,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 
